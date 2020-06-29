6907c6cf90217eb4175b8921fe8c83e37b8cf647
Liberbell <liberbell@gmail.com>
2020-06-29 09:10:17 +0900

    client2

--- client.py
+++ client.py
@@ -6,15 +6,17 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 
 	s.connect((socket.gethostname(), 4571))
 
-	custom_file = open('client_files/received_file.txt', 'wb')
+	# custom_file = open('client_files/received_file.txt', 'wb')
 
 	while True:
-		
-		data = s.recv(40960)
-
-		if not data:
+		msg = s.recv(1024)
+		# data = s.recv(40960)
+		if not msg:
 			print('No messages from the server. Closing the connection...')
 			break
+		# if not data:
+		# 	print('No messages from the server. Closing the connection...')
+		# 	break
 
 		custom_file.write(data)
 		print('Batch of data written to file...')
