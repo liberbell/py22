2aecd43de75478a3e9cc17f57475ff47f85e27a7
Liberbell <liberbell@gmail.com>
2020-07-01 08:37:36 +0900

    server4

--- files_and_images/server.py
+++ files_and_images/server.py
@@ -16,3 +16,5 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     while(custom_data):
         client.send(custom_data)
         custom_data = custom_file.read(40960)
+
+    print("Custom file successfully sent.")
