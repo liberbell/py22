51e40bd1bf35e7b42177a74e8ee2a1ac3d22f0bc
Liberbell <liberbell@gmail.com>
2020-07-01 08:50:50 +0900

    client4

--- files_and_images/client.py
+++ files_and_images/client.py
@@ -3,3 +3,12 @@ import socket
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as r:
     r.connect(("localhost", 4571))
     custom_file = open("client_files/recieved_file.txt", "wb")
+
+    while True:
+        data = r.recv(40960)
+        if not data:
+            print("No messages from the server. Closing the connection....")
+            break
+        
+        custom_file.write(data)
+        print("Batch of data written to file....")
