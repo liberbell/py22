7ae8ae7234cbfd32ba2b0789732e243ed2217e4c
Liberbell <liberbell@gmail.com>
2020-07-02 09:35:36 +0900

    server4

--- chat/server.py
+++ chat/server.py
@@ -4,3 +4,5 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     server_name = input("Enter your name: ")
 
     sock.bind(('localhost', 4571))
+    sock.listen(5)
+    print(server_name, ' is up. Listening for connection....')
