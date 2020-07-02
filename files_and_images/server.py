ce48f6ffedce317ce05f6f6165f695fdcdec8923
Liberbell <liberbell@gmail.com>
2020-07-02 09:07:03 +0900

    server2

--- files_and_images/server.py
+++ files_and_images/server.py
@@ -11,11 +11,19 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     print("Connection to", address, "established\n")
     print("Client object:", client, "\n")
 
-    custom_file = open("server_files/meditations.txt", "rb")
-    custom_data = custom_file.read(40960)
+    # custom_file = open("server_files/meditations.txt", "rb")
+    # custom_data = custom_file.read(40960)
 
-    while(custom_data):
-        client.send(custom_data)
-        custom_data = custom_file.read(40960)
+    # while(custom_data):
+    #     client.send(custom_data)
+    #     custom_data = custom_file.read(40960)
 
-    print("Custom file successfully sent.")
+    # print("Custom file successfully sent.")
+    with open('server_files/dog.jpg', 'rb') as image_file:
+        image_batch = image_file.read(40960)
+
+        while (image_batch):
+            client.send(image_batch)
+            image_batch = image_file.read(40960)
+
+print("Image sent successfully.")
