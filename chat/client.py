4b9c38af63b32b25f9759278a50c523c64b5d0c3
Liberbell <liberbell@gmail.com>
2020-07-03 08:41:33 +0900

    client3

--- chat/client.py
+++ chat/client.py
@@ -2,3 +2,6 @@ import socket
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     client_name = input("Enter your name: ")
+
+    sock.connect(("localhost", 4571))
+    sock.send(client_name.encode())
