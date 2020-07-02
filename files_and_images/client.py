3c56cbce6a9a4975c67b63a8452f7878b5bc45b5
Liberbell <liberbell@gmail.com>
2020-07-02 09:10:47 +0900

    client2

--- files_and_images/client.py
+++ files_and_images/client.py
@@ -3,15 +3,15 @@ from PIL import Image
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as r:
     r.connect(("localhost", 4571))
-    custom_file = open("client_files/received_file.txt", "wb")
-
-    while True:
-        data = r.recv(40960)
-        if not data:
-            print("No messages from the server. Closing the connection....")
-            break
-        
-        custom_file.write(data)
-        print("Batch of data written to file....")
-        
-    custom_file.close()
+    # custom_file = open("client_files/received_file.txt", "wb")
+    with open('client_files/received_file.jpg', 'wb') as image_file:
+        while True:
+            data = r.recv(40960)
+            if not data:
+                print("No messages from the server. Closing the connection....")
+                break
+            
+            custom_file.write(data)
+            print("Batch of data written to file....")
+            
+        custom_file.close()
