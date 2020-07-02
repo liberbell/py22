36f3116068f3980cea8ad29a2216878c0eb09f3b
Liberbell <liberbell@gmail.com>
2020-07-03 08:43:11 +0900

    client4

--- chat/client.py
+++ chat/client.py
@@ -5,3 +5,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
 
     sock.connect(("localhost", 4571))
     sock.send(client_name.encode())
+
+    server_name_raw = sock.recv(1024)
+    server_name = server_name_raw.decode()
+    print("You have connected to the server %s", %server_name)
