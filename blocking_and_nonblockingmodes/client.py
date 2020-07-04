03b806cd64713fa62c46f84825a79ef27b3d91d1
Liberbell <liberbell@gmail.com>
2020-07-04 15:28:14 +0900

    client5

--- blocking_and_nonblockingmodes/client.py
+++ blocking_and_nonblockingmodes/client.py
@@ -2,7 +2,7 @@ import socket
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     sock.connect(("localhost", 4571))
-    sock.setblocking(Ture)
+    sock.setblocking(True)
 
     data = bytes("Hello Server\n", "UTF-8") * 1024 * 1024 * 10
     print("Size of data sent: %1 bytes." %len(data))
