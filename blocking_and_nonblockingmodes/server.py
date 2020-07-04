aced5cd17dee3e7d3b3f0687c1883708d2a86f0a
Liberbell <liberbell@gmail.com>
2020-07-04 12:43:37 +0900

    server4

--- blocking_and_nonblockingmodes/server.py
+++ blocking_and_nonblockingmodes/server.py
@@ -4,3 +4,8 @@ from datetime import datetime
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind(("localhost", 4571))
     s.listen(5)
+
+    print("Server is up. Listening for connections...")
+
+    client, address = s.accept()
+    print("Connection to: ", address, "established.\n")
