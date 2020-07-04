4979c14af82cfdfd96c9df23f39309b2dffc18c2
Liberbell <liberbell@gmail.com>
2020-07-04 12:46:17 +0900

    server6

--- blocking_and_nonblockingmodes/server.py
+++ blocking_and_nonblockingmodes/server.py
@@ -5,8 +5,12 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind(("localhost", 4571))
     s.listen(5)
 
-    print("Server is up. Listening for connections...")
+    print("Server is up. Listening for connections...\n")
 
     client, address = s.accept()
     print("Connection to: ", address, "established.\n")
     print("Client object: ", client, "\n")
+
+    start_time = datetime.now()
+    data = client.recv(1024)
+    total_received_size = len(data)
