864a474254a32567a0856dd9ee05697cc6372c9a
Liberbell <liberbell@gmail.com>
2020-07-05 15:25:42 +0900

    server3

--- rss_feed/server.py
+++ rss_feed/server.py
@@ -1,2 +1,7 @@
 import socket
 import feedparser
+
+feed = feedparser.parse("https://support.microsoft.com/app/content/api/content/feeds/sap/en-us/32d322a8-acae-202d-e9a9-7371dccf381b/rss")
+
+feed_decode = feed.decode()
+print(feed_decode)
