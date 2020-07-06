fbc257b7207c2335d36f1c0dfce11b5e7e7aebe7
Liberbell <liberbell@gmail.com>
2020-07-06 09:11:50 +0900

    client3

--- rss_feed/client.py
+++ rss_feed/client.py
@@ -2,3 +2,5 @@ import feedparser
 import socket
 
 feed = feedparser.parse(("https://news.un.org/feed/subscribe/\en/news/topic/economic-development/feed/rss.xml"))
+
+print("Number of RSS posts: ", len(feed.entries), "\n")
