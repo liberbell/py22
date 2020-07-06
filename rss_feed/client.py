926dffa80652f2dde4b6d3462d6324d459fd8907
Liberbell <liberbell@gmail.com>
2020-07-06 09:18:14 +0900

    client7

--- rss_feed/client.py
+++ rss_feed/client.py
@@ -5,5 +5,9 @@ import socket
 feed = feedparser.parse("https://news.un.org/feed/subscribe/en/news/topic/economic-development/feed/rss.xml")
 # feed = feedparser.parse("https://support.microsoft.com/app/content/api/content/feeds/sap/en-us/32d322a8-acae-202d-e9a9-7371dccf381b/rss")
 
-
 print("Number of RSS posts: ", len(feed.entries), "\n")
+
+while True:
+    print("###### Begin Feed ######\n\n")
+    for post in feed.entries:
+        print(post.title, ":\n", post.link, "\n\n")
