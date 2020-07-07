133fd585c74b105c9c9e2568ae2ea62e08678c84
Liberbell <liberbell@gmail.com>
2020-07-07 09:08:55 +0900

    server1

--- udp_sockets/server.py
+++ udp_sockets/server.py
@@ -10,3 +10,8 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
     while True:
         message = bytes("Message #" + str(i), "UTF-8")
         sock.sendto(message, ("localhost", 37020))
+
+        print("Message sent!")
+        time.sleep(5)
+
+        i += 1
