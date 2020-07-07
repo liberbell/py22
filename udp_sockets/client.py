99e8d65b393db68784506fb4290b6b0baf08ccd9
Liberbell <liberbell@gmail.com>
2020-07-07 09:13:16 +0900

    client3

--- udp_sockets/client.py
+++ udp_sockets/client.py
@@ -2,3 +2,7 @@ import socket
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM)as sock:
     sock.bind(("", 37020))
+
+    while True:
+        data, addr = sock.recvfrom(1024)
+        print("Received message: ", data)
