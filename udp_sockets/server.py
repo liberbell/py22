80064c093ee9007b4b88f441c0e902eaf03cf5c4
Liberbell <liberbell@gmail.com>
2020-07-07 09:15:09 +0900

    server2

--- udp_sockets/server.py
+++ udp_sockets/server.py
@@ -2,7 +2,7 @@ import time
 import socket
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
-    sock.connect(0.2)
+    sock.settimeout(0.2)
     sock.bind(("localhost", 4571))
 
     i = 1
